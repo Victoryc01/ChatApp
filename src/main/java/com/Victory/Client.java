8b9ec3ba5cfcdcc71888888de18d281ac6d9f498
Victory <blazehoodz@gmail.com>
2023-09-20 09:51:01 +0100

    first commit

--- src/main/java/com/Victory/Client.java
+++ src/main/java/com/Victory/Client.java
@@ -7,51 +7,25 @@ public class Client {
     public static void main(String[] args) {
 
         try {
-            Socket s = new Socket("127.0.0.1", 1201);
+            Socket s = new Socket("127.0.0.1",1201);
 
             DataInputStream din = new DataInputStream(s.getInputStream());
             DataOutputStream dout = new DataOutputStream(s.getOutputStream());
 
-            // Thread for receiving messages from the server
-            Thread receiveThread = new Thread(() -> {
-                try {
-                    while (true) {
-                        String msgin = din.readUTF();
-                        if (msgin.equals("end")) {
-                            System.out.println("Server disconnected.");
-                            break;
-                        }
-                        System.out.println("Server: " + msgin);
-                    }
-                } catch (IOException e) {
-                    System.out.println("Client error: " + e);
-                }
-            });
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            String msgin="", msgout ="";
 
-            // Thread for sending messages to the server
-            Thread sendThread = new Thread(() -> {
-                try {
-                    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-                    String msgout;
-                    while (true) {
-                        msgout = br.readLine();
-                        dout.writeUTF(msgout);
-                        dout.flush();
-                        if (msgout.equals("end")) {
-                            System.out.println("Client disconnected.");
-                            break;
-                        }
-                    }
-                } catch (IOException e) {
-                    System.out.println("Client error: " + e);
-                }
-            });
-
-            receiveThread.start();
-            sendThread.start();
+            while (!msgin.equals("end")){
+                msgout = br.readLine();
+                dout.writeUTF(msgout);
+                dout.flush();
+                msgin = din.readUTF();
+                System.out.println("Server: "+msgin);
+            }
 
         } catch (Exception e) {
-            System.out.println("Client error: " + e);
+            // throw new RuntimeException(e);
+            System.out.println("check port"+ e);
         }
     }
 }
